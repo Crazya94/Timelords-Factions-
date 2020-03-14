1218e74e4a5d9ddd888769e1a1cbccb79b073442
Ogliss <randomdragon2@hotmail.com>
2020-03-14 23:53:06 +0000

    Added Regeneration

---
+++ Source/Timelords/Comp_TimeLord.cs
@@ -0,0 +1,47 @@
+﻿using AlienRace;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace TimeLords
+{
+    public class ExtendedTraitEntry
+    {
+        public TraitDef def = null;
+        public int degree = 0;
+        public float Chance = 1f;
+        public bool replaceIfFull = true;
+    }
+    // Token: 0x02000241 RID: 577
+    public class CompProperties_TimeLord : CompProperties
+    {
+        // Token: 0x06000FEE RID: 4078 RVA: 0x0005AFE0 File Offset: 0x000591E0
+        public CompProperties_TimeLord()
+        {
+            this.compClass = typeof(Comp_TimeLord);
+        }
+
+    }
+    // Token: 0x02000242 RID: 578
+    public class Comp_TimeLord : ThingComp
+    {
+        public int TimesRegenerated = 0;
+        public CompProperties_TimeLord Props
+        {
+            get
+            {
+                return (CompProperties_TimeLord)this.props;
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref this.TimesRegenerated, "TimesRegenerated", 0);
+        }
+    }
+}
---
+++ Source/Timelords/DeathActionWorker_TimeLord.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace TimeLords
+{
+	// Token: 0x02000005 RID: 5
+	public class DeathActionWorker_TimeLord : DeathActionWorker
+	{
+		// Token: 0x06000005 RID: 5 RVA: 0x000020B4 File Offset: 0x000002B4
+		public override void PawnDied(Corpse corpse)
+		{
+			float value = (float)corpse.InnerPawn.ageTracker.AgeBiologicalYears;
+			float value2 = (float)corpse.InnerPawn.ageTracker.AgeChronologicalYears;
+			Name name = corpse.InnerPawn.Name;
+			Pawn_PlayerSettings playerSettings = corpse.InnerPawn.playerSettings;
+			Pawn_TrainingTracker training = corpse.InnerPawn.training;
+			Pawn_HealthTracker health = corpse.InnerPawn.health;
+			Pawn_RecordsTracker records = corpse.InnerPawn.records;
+			Pawn_RelationsTracker relations = corpse.InnerPawn.relations;
+			Pawn_SkillTracker skills = corpse.InnerPawn.skills;
+			Faction faction = corpse.InnerPawn.Faction;
+			Comp_TimeLord timeLord = corpse.InnerPawn.TryGetComp<Comp_TimeLord>();
+			if (timeLord!=null)
+			{
+				if (timeLord.TimesRegenerated<12)
+				{
+					timeLord.TimesRegenerated++;
+					Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(corpse.InnerPawn.kindDef, faction, PawnGenerationContext.NonPlayer, -1, false, false, false, false, true, false, 0f, false, false, false, false, false, false, false, false, 0, null, 1, null, null, null));
+					GenSpawn.Spawn(pawn, corpse.Position, corpse.Map, Rot4.Random);
+					pawn.Name = name;
+					pawn.relations = relations;
+					pawn.training = training;
+					pawn.records = records;
+					pawn.skills = skills;
+					pawn.playerSettings = playerSettings;
+					corpse.Destroy(0);
+				}
+			}
+
+		}
+	}
+}
